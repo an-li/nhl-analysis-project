6c85941e05136c55a087e1d99e7b50fa41553bd3
an-li <31196165+an-li@users.noreply.github.com>
2022-10-04 15:03:08 -0400

    Add CSV export

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from datetime import datetime
 from ift6758.features.data_extractor import extract_and_cleanup_play_data
 
 if __name__ == "__main__":
-    all_plays_df = extract_and_cleanup_play_data(datetime(2016, 10, 12), datetime(2021, 7, 7), ['Shot', 'Goal'],
+    start_date = datetime(2016, 10, 12)
+    end_date = datetime(2021, 7, 7)
+    all_plays_df = extract_and_cleanup_play_data(start_date, end_date, ['Shot', 'Goal'],
                                                  ['gameId', 'season', 'gameType', 'dateTime', 'team', 'event',
                                                   'secondaryType', 'description', 'period',
                                                   'periodType', 'periodTime', 'players', 'strength', 'emptyNet', 'x',
                                                   'y', 'rinkSide', 'distanceToGoal'])
+    all_plays_df.to_csv(f'plays_{start_date.strftime("%Y%m%d")}_{end_date.strftime("%Y%m%d")}.csv', index=False)
