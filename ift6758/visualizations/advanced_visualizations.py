d0b6f11b67952c394b597eb6f3aa47716afe9c4b
an-li <31196165+an-li@users.noreply.github.com>
2022-10-24 09:08:28 -0400

    Update all shot maps and grouped advanced_visualizations into methods

--- ift6758/visualizations/advanced_visualizations.py
+++ ift6758/visualizations/advanced_visualizations.py
@@ -22,62 +22,23 @@ def generate_static_shot_map(plays_df: pd.DataFrame, team: str = None, season: i
 
     """
 
-    global_df = filter_by_team_and_season(plays_df, season_filter=season)
-    global_matrix = generate_shot_map_matrix(global_df, bin_size=group_feet)
-    global_shots_per_game = global_matrix.sum().sum()
-    global_goals_per_game = get_goals_per_game(global_df)
-
     fig = go.Figure()
 
+    global_df = filter_by_team_and_season(plays_df, season_filter=season)
+    global_matrix, global_shots_per_game, global_goals_per_game = _get_matrix_and_stats_per_game(global_df, group_feet)
+
     team_df = filter_by_team_and_season(plays_df, team, season)
-    team_matrix = generate_shot_map_matrix(team_df, bin_size=group_feet)
-    team_shots_per_game = team_matrix.sum().sum()
-    team_goals_per_game = get_goals_per_game(team_df, team_filter=team, season_filter=season)
+    team_matrix, team_shots_per_game, team_goals_per_game = _get_matrix_and_stats_per_game(team_df, group_feet)
 
     difference_matrix = subtract_and_align_matrices(team_matrix, global_matrix, 0.0)
     # Sort by decreasing distance from center ice so center ice appears at the bottom of the graph
     difference_matrix.sort_index(ascending=False, kind='mergesort', inplace=True)
 
-    largest_diff = max(abs(difference_matrix.min().min()), abs(difference_matrix.max().max()))
-
-    fig.add_trace(go.Contour(z=difference_matrix, showscale=True, connectgaps=False,
-                             colorscale=[[0, 'rgb(0,0,255)'], [0.5, 'rgb(255,255,255)'], [1,
-                                                                                          'rgb(255,0,0)']],
-                             x=difference_matrix.columns, y=difference_matrix.index, zmin=-1 * largest_diff,
-                             zmax=largest_diff, line_smoothing=1.3))
+    fig.add_trace(_create_contour_map(difference_matrix, team, True))
 
     # Update plot sizing
-    fig.update_layout(
-        width=800,
-        height=1050,
-        autosize=False,
-        margin=dict(t=230, b=0, l=0, r=0),
-        title=f'Plan des tirs de l\'équipe {team} et de la saison {season}<br>{round(team_shots_per_game, 2)} tirs par match ({round((team_shots_per_game - global_shots_per_game) / global_shots_per_game * 100, 2)}% par rapport à la moyenne)<br>{round(team_goals_per_game, 2)} buts par match ({round((team_goals_per_game - global_goals_per_game) / global_goals_per_game * 100, 2)}% par rapport à la moyenne)',
-        template="plotly_white",
-    )
-
-    # Add axes title
-    fig.update_xaxes(title_text='Distance du centre de la patinoire (pi)')
-    fig.update_yaxes(title_text='Distance du centre de la glace au but (pi)')
-
-    # Update 3D scene options
-    fig.update_scenes(
-        aspectratio=dict(x=1, y=1, z=0.7),
-        aspectmode="auto"
-    )
-
-    img = Image.open('figures/nhl_rink_offensive.png')
-    fig.add_layout_image(
-        dict(
-            source=img,
-            xref="paper", yref="paper",
-            x=0.5, y=0,
-            sizex=1, sizey=1,
-            xanchor="center",
-            yanchor="bottom",
-            opacity=0.3,
-        )
-    )
+    _update_figure_layout(fig, _create_title(team, season, global_shots_per_game, global_goals_per_game,
+                                             team_shots_per_game, team_goals_per_game))
 
     if plot:
         fig.show()
@@ -118,28 +79,20 @@ def generate_interactive_shot_map(plays_df: pd.DataFrame, save: bool = True, plo
 
     for current_season in season_list:
         global_df = filter_by_team_and_season(plays_df, season_filter=current_season)
-        global_matrix = generate_shot_map_matrix(global_df, bin_size=group_feet)
-        global_shots_per_game = global_matrix.sum().sum()
-        global_goals_per_game = get_goals_per_game(global_df)
+        global_matrix, global_shots_per_game, global_goals_per_game = _get_matrix_and_stats_per_game(global_df,
+                                                                                                     group_feet)
+
         for current_team in team_list:
             team_df = filter_by_team_and_season(plays_df, current_team, current_season)
             if len(team_df) > 0:
-                team_matrix = generate_shot_map_matrix(team_df, bin_size=group_feet)
-                team_shots_per_game = team_matrix.sum().sum()
-                team_goals_per_game = get_goals_per_game(team_df, team_filter=current_team,
-                                                         season_filter=current_season)
+                team_matrix, team_shots_per_game, team_goals_per_game = _get_matrix_and_stats_per_game(team_df,
+                                                                                                       group_feet)
 
                 difference_matrix = subtract_and_align_matrices(team_matrix, global_matrix, 0.0)
                 # Sort by decreasing distance from center ice so center ice appears at the bottom of the graph
                 difference_matrix.sort_index(ascending=False, kind='mergesort', inplace=True)
 
-                largest_diff = max(abs(difference_matrix.min().min()), abs(difference_matrix.max().max()))
-
-                fig.add_trace(go.Contour(z=difference_matrix, showscale=True, connectgaps=False,
-                                         colorscale=[[0, 'rgb(0,0,255)'], [0.5, 'rgb(255,255,255)'], [1,
-                                                                                                      'rgb(255,0,0)']],
-                                         x=difference_matrix.columns, y=difference_matrix.index, zmin=-1 * largest_diff,
-                                         zmax=largest_diff, line_smoothing=1.3, visible=False))
+                fig.add_trace(_create_contour_map(difference_matrix, current_team, False))
 
                 visible_copy = visible.copy()
                 visible_copy[index] = True
@@ -148,31 +101,143 @@ def generate_interactive_shot_map(plays_df: pd.DataFrame, save: bool = True, plo
                                     method='update',
                                     args=[{'visible': visible_copy},
                                           {
-                                              'title': f'Plan des tirs de l\'équipe {current_team} et de la saison {current_season}<br>{round(team_shots_per_game, 2)} tirs par match ({round((team_shots_per_game - global_shots_per_game) / global_shots_per_game * 100, 2)}% par rapport à la moyenne)<br>{round(team_goals_per_game, 2)} buts par match ({round((team_goals_per_game - global_goals_per_game) / global_goals_per_game * 100, 2)}% par rapport à la moyenne)',
+                                              'title': _create_title(current_team, current_season,
+                                                                     global_shots_per_game, global_goals_per_game,
+                                                                     team_shots_per_game, team_goals_per_game),
                                               'showlegend': False}]))
 
                 index += 1
 
     # Update plot sizing
+    _update_figure_layout(fig, 'Plan des tirs en fonction de l\'équipe et de la saison sélectionnée')
+
+    _create_dropdown(fig, buttons)
+
+    if plot:
+        fig.show()
+    if save:
+        with open(path_to_save + 'interactive_shot_map.html', 'wb') as f:
+            f.write(fig.to_html().encode('UTF-8'))
+
+
+def _get_matrix_and_stats_per_game(plays_df: pd.DataFrame, group_feet: int) -> (pd.DataFrame, float, float):
+    """
+    Get shot matrix and number of shots and goals per game
+
+    Args:
+        plays_df: Data frame of plays for a specific season and/or team
+        group_feet: Integer to regroup distances
+
+    Returns:
+        Tuple containing:
+            matrix: Shot matrix
+            shots_per_game: Number of shots per game
+            goals_per_game: Number of goals per game
+
+    """
+    matrix = generate_shot_map_matrix(plays_df, bin_size=group_feet)
+    shots_per_game = matrix.sum().sum()
+    goals_per_game = get_goals_per_game(plays_df)
+
+    return matrix, shots_per_game, goals_per_game
+
+
+def _create_title(team: str, season: int, global_shots_per_game: float, global_goals_per_game: float,
+                  team_shots_per_game: float, team_goals_per_game: float) -> str:
+    """
+    Create title with team name and season, as well as number of shots and goals by hour by team and compared to NHL average
+    Assume each game is one hour long
+
+    Args:
+        team: Name of team
+        season: Season
+        global_shots_per_game: NHL average number of shots per game of season
+        global_goals_per_game: NHL average number of goals scored per game of season
+        team_shots_per_game: Team average number of shots per game of season
+        team_goals_per_game: Team average number of goals scored per game of season
+
+    Returns:
+        Title containing team name and season, as well as number of shots and goals by hour by team and compared to NHL average
+    """
+
+    return f'Plan des tirs de l\'équipe {team} et de la saison {season}<br>{round(team_shots_per_game, 2)} tirs par heure ({round((team_shots_per_game - global_shots_per_game) / global_shots_per_game * 100, 2)}% par rapport à la moyenne)<br>{round(team_goals_per_game, 2)} buts par heure ({round((team_goals_per_game - global_goals_per_game) / global_goals_per_game * 100, 2)}% par rapport à la moyenne)'
+
+
+def _create_contour_map(difference_matrix: pd.DataFrame, name: str, visible: bool = True) -> go.Contour:
+    """
+    Create a contour map with the difference matrix, can optionally make it invisible
+
+    Args:
+        difference_matrix: Matrix of differences between global data and team data
+        name: Name of series
+        visible: True to make it visible by default, False otherwise
+
+    Returns:
+        Contour map with the difference matrix
+    """
+
+    largest_diff = max(abs(difference_matrix.min().min()), abs(difference_matrix.max().max()))
+
+    return go.Contour(name=name, z=difference_matrix, showscale=True, connectgaps=True,
+                      colorscale=[[0, 'rgb(0,0,255)'], [0.5, 'rgb(255,255,255)'], [1,
+                                                                                   'rgb(255,0,0)']],
+                      x=difference_matrix.columns, y=difference_matrix.index, zmin=-1 * largest_diff,
+                      zmax=largest_diff, line_smoothing=1.3, visible=visible)
+
+
+def _update_figure_layout(fig, title: str):
+    """
+    Update layout for plot with title
+
+    Args:
+        fig: Figure
+        title: Title of plot
+
+    Returns:
+
+    """
 
     fig.update_layout(
         width=800,
         height=1050,
         autosize=False,
         margin=dict(t=230, b=0, l=0, r=0),
-        title='Plan des tirs en fonction de l\'équipe et de la saison sélectionnée',
+        title=title,
         template="plotly_white",
     )
-
     # Add axes title
     fig.update_xaxes(title_text='Distance du centre de la patinoire (pi)')
     fig.update_yaxes(title_text='Distance du centre de la glace au but (pi)')
-
     # Update 3D scene options
     fig.update_scenes(
         aspectratio=dict(x=1, y=1, z=0.7),
         aspectmode="auto"
     )
+    img = Image.open('figures/nhl_rink_offensive.png')
+    fig.add_layout_image(
+        dict(
+            source=img,
+            xref="paper", yref="paper",
+            x=0.5, y=0,
+            sizex=1, sizey=1,
+            xanchor="center",
+            yanchor="bottom",
+            opacity=0.3,
+        )
+    )
+
+
+def _create_dropdown(fig, buttons):
+    """
+    Create dropdown for figure
+
+    Args:
+        fig: Figure
+        buttons: List of buttons to add to dropdown
+
+    Returns:
+
+    """
 
     fig.update_layout(
         updatemenus=[go.layout.Updatemenu(
@@ -188,22 +253,3 @@ def generate_interactive_shot_map(plays_df: pd.DataFrame, save: bool = True, plo
         )
         ]
     )
-
-    img = Image.open('figures/nhl_rink_offensive.png')
-    fig.add_layout_image(
-        dict(
-            source=img,
-            xref="paper", yref="paper",
-            x=0.5, y=0,
-            sizex=1, sizey=1,
-            xanchor="center",
-            yanchor="bottom",
-            opacity=0.3,
-        )
-    )
-
-    if plot:
-        fig.show()
-    if save:
-        with open(path_to_save + 'interactive_shot_map.html', 'wb') as f:
-            f.write(fig.to_html().encode('UTF-8'))
