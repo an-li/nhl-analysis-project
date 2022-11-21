a34bd5ca9960f69f7bd0400e9c0e80681cd8d851
an-li <31196165+an-li@users.noreply.github.com>
2022-11-21 05:48:37 -0500

    Add a retry decorator for API calls

--- ift6758/api/nhl_api_service.py
+++ ift6758/api/nhl_api_service.py
@@ -1,10 +1,12 @@
 from datetime import datetime
 
 import requests
+from retry import retry
 
 API_BASE_URL = 'https://statsapi.web.nhl.com/api/v1'
 
 
+@retry(requests.exceptions.RequestException, tries=5, delay=2, jitter=(0, 1), backoff=2)
 def get_schedule_by_date_range(start_date: datetime, end_date: datetime) -> dict:
     """
     Get schedule of NHL games between specific dates
@@ -18,11 +20,14 @@ def get_schedule_by_date_range(start_date: datetime, end_date: datetime) -> dict
     """
     try:
         r = requests.get(f'{API_BASE_URL}/schedule?startDate={start_date.strftime("%Y-%m-%d")}&endDate={end_date.strftime("%Y-%m-%d")}')
+        if len(r.content) == 0:
+            raise requests.exceptions.RequestException("Cannot decode empty response!")
         return r.json()
     except requests.exceptions.RequestException as e:
         print(f'Cannot get schedule for dates from {start_date.strftime("%Y-%m-%d")} to {end_date.strftime("%Y-%m-%d")}, {e}')
 
 
+@retry(requests.exceptions.RequestException, tries=5, delay=2, jitter=(0, 1), backoff=2)
 def get_game_live_feed(game_id: str) -> dict:
     """
     Get live feed of NHL game with specified ID
@@ -35,6 +40,8 @@ def get_game_live_feed(game_id: str) -> dict:
     """
     try:
         r = requests.get(f'{API_BASE_URL}/game/{game_id}/feed/live')
+        if len(r.content) == 0:
+            raise requests.exceptions.RequestException("Cannot decode empty response!")
         return r.json()
     except requests.exceptions.RequestException as e:
         print(f'Cannot get feed for game {game_id}, {e}')
