d133fd7ca499368985e740642210f5422a4a4ee3
dragonpan2 <panbeining@hotmail.com>
2022-11-30 21:37:23 -0500

    add launch code for build and run sh files

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "TODO: fill in the docker build command"
+docker build --tag serving . -f Dockfile.serving
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "TODO: fill in the docker run command"
+docker run -p 8080:8080/tcp -p 8080:8080/udp serving
