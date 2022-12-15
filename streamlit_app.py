eac56f20e9b2cc7f18a820dd60bd859f807fe95f
an-li <31196165+an-li@users.noreply.github.com>
2022-12-15 16:05:49 -0500

    Reformat streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
+import json
 import logging
 import os
 
-import streamlit as st
-import pandas as pd
-import numpy as np
 import requests
-import json
+import streamlit as st
 
 from serving.game_client import GameClient
 from serving.logger import Logger
@@ -25,57 +23,64 @@ game_client = GameClient(logger)
 
 PORT = "8080"
 IP = "http://127.0.0.1:"
-address = IP+PORT
+address = IP + PORT
 
-def pingGame(game_id):
+
+def ping_game(game_id):
     shots_goals, last_event = game_client.load_shots_and_last_event(game_id)
     r = requests.post(
-        address+"/predict", 
-        json=json.loads(shots_goals.to_json())
+        address + "/predict",
+        json={
+            'data': shots_goals.to_dict(orient='records')
+        }
     )
     print(r.json())
 
-def pingServer():
+
+def ping_server():
     r = requests.get(
-        address+"/ping")
+        address + "/ping")
     print(r)
 
-def downloadModel(workspace, model, version):
-    jsonString = '{ "workspace":{workspace}, "model":{workspace}, "version":{workspace}}'.format(workspace=workspace, model=model, version=version)
+
+def download_model(workspace, model, version):
     r = requests.post(
-        address+"/predict", 
-        json=json.loads(jsonString)
+        address + "/download_registry_model",
+        json={
+            'workspace': workspace,
+            'model': model,
+            'version': version
+        }
     )
     print(r)
 
 
 with st.sidebar:
     # TODO: Add input for the sidebar
-    workplaceOption = st.selectbox(
-    'Workplace',
-    ('ift6758a-a22-g3-projet',))
+    workspace_option = st.selectbox(
+        'Workspace',
+        ('ift6758a-a22-g3-projet',))
 
-    modelOption = st.selectbox(
-    'Model',
-    ('XGBoost_All_Features', 'XGBoost_KBest_25_f_classif', 'XGBoost_KBest_25_mutual_info_classif'))
+    model_option = st.selectbox(
+        'Model',
+        ('XGBoost_All_Features', 'XGBoost_KBest_25_f_classif', 'XGBoost_KBest_25_mutual_info_classif'))
 
-    versionOption = st.selectbox(
-    'Version',
-    ('1.0.0',))
+    version_option = st.selectbox(
+        'Version',
+        ('1.0.0',))
 
     if st.button('Get Model'):
-        downloadModel(workplaceOption, modelOption, versionOption)
-    
-    #TODO: Remove this before handing the project
-    if st.button('DEBUG ONLY: Test Ping Server'):
-        pingServer()
+        download_model(workspace_option, model_option, version_option)
 
+    # TODO: Remove this before handing the project
+    if st.button('DEBUG ONLY: Test Ping Server'):
+        ping_server()
 
 with st.container():
     # TODO: Add Game ID input
     st.text_input("Game Id", key="gameId")
     if st.button('Ping Game'):
-        pingGame(st.session_state.gameId)
+        ping_game(st.session_state.gameId)
 
 with st.container():
     # TODO: Add Game info and predictions
@@ -84,5 +89,3 @@ with st.container():
 with st.container():
     # TODO: Add data used for predictions
     pass
-
-
