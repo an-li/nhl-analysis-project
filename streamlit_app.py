12291d5bcd787403bd854140f2d827b38a7a6512
an-li <31196165+an-li@users.noreply.github.com>
2022-12-16 12:28:16 -0500

    Implemented prediction logic without the visuals

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import json
 import logging
 import os
 
+import pandas as pd
 import requests
 import streamlit as st
 
@@ -25,16 +26,46 @@ PORT = "8080"
 IP = "http://127.0.0.1:"
 address = IP + PORT
 
+# Save name of default model
+if not st.session_state.get('model'):
+    st.session_state.model = 'XGBoost_KBest_25_f_classif'
+
+with open('features_by_model.json', 'rb') as fp:
+    features_by_model = json.load(fp)
+
 
 def ping_game(game_id):
+    logger.auto_log(f'Predicting data for game {game_id} using model {st.session_state.model}', is_print=True)
     shots_goals, last_event = game_client.load_shots_and_last_event(game_id)
-    r = requests.post(
-        address + "/predict",
-        json={
-            'data': shots_goals.to_dict(orient='records')
-        }
-    )
-    print(r.json())
+
+    # If some data already exists for game, only need to predict remaining indices
+    if st.session_state.get(f'last_event_{game_id}'):
+        shots_goals = shots_goals[shots_goals['eventIdx'] > st.session_state.get(f'last_event_{game_id}')['eventIdx']]
+
+    # Perform predictions when there is at least one game event
+    if len(shots_goals) > 0:
+        r = requests.post(
+            address + "/predict",
+            json={
+                'features': features_by_model[st.session_state.model]['features'],
+                'features_to_one_hot': features_by_model[st.session_state.model]['features_to_one_hot'],
+                'one_hot_features': features_by_model[st.session_state.model]['one_hot_features'],
+                'data': shots_goals.to_dict(orient='records')
+            }
+        )
+        if r.status_code == 200:
+            predictions = pd.DataFrame(r.json()['predictions'])
+            shots_goals_with_predictions = shots_goals.merge(predictions, how='left', on='eventIdx')
+            if st.session_state.get(f'predictions_{game_id}'):
+                st.session_state[f'predictions_{game_id}'] = pd.concat(
+                    [st.session_state[f'predictions_{game_id}'], shots_goals_with_predictions], ignore_index=True)
+            else:
+                st.session_state[f'predictions_{game_id}'] = shots_goals_with_predictions
+            print(shots_goals_with_predictions.to_dict(orient='records'))
+
+    # Finally, save last event for current game
+    if last_event:
+        st.session_state[f'last_event_{game_id}'] = last_event
 
 
 def ping_server():
@@ -52,7 +83,13 @@ def download_model(workspace, model, version):
             'version': version
         }
     )
-    print(r)
+    if r.status_code == 200:
+        # When the model is changed, clear the session state
+        for key in st.session_state.keys():
+            del st.session_state[key]
+
+        st.session_state.model = model
+        logger.auto_log(f'Model {model} loaded successfully', is_print=True)
 
 
 with st.sidebar:
@@ -63,7 +100,7 @@ with st.sidebar:
 
     model_option = st.selectbox(
         'Model',
-        ('XGBoost_All_Features', 'XGBoost_KBest_25_f_classif', 'XGBoost_KBest_25_mutual_info_classif'))
+        ('XGBoost_KBest_25_f_classif', 'XGBoost_All_Features', 'XGBoost_KBest_25_mutual_info_classif'))
 
     version_option = st.selectbox(
         'Version',
@@ -78,9 +115,9 @@ with st.sidebar:
 
 with st.container():
     # TODO: Add Game ID input
-    st.text_input("Game Id", key="gameId")
+    st.text_input("Game Id", key="game_id")
     if st.button('Ping Game'):
-        ping_game(st.session_state.gameId)
+        ping_game(st.session_state.game_id)
 
 with st.container():
     # TODO: Add Game info and predictions
